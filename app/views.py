78a199717f961dc601f5e277350339d702c66657
elpadrino94 <celestindjato5@gmail.com>
2026-05-15 15:17:56 +0000

    Mise en place de home page, post-list, les vues, le post_list css et base.html

--- app/views.py
+++ app/views.py
@@ -15,5 +15,19 @@ class HomeView(ListView):
             status='published'
             ).select_related('category', 'author').order_by('-published_at')
 
-class PostListView(TemplateView):
+    def get_context_data(self, **kargs):
+        context = super().get_context_data(**kargs)
+        context['recent_posts'] = Post.objects.filter(status='published').order_by('-published_at')[:4]
+        return context
+
+
+
+class PostListView(ListView):
+    model = Post
     template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            status='published'
+            ).select_related('category', 'author').order_by('-published_at')
