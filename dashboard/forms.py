c1a9508f0eac85e6882913c5f0c4381e5992669e
elpadrino94 <celestindjato5@gmail.com>
2026-06-10 09:40:43 +0000

    Amélioration des formulaires d'ajout de catégorie et d'article avec des widgets personnalisés et des styles CSS optimisés

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -14,4 +14,14 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'excerpt', 'content', 'image', 'status', 'category', 'meta_title', 'meta_description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Entrez le titre de votre article'}),
+            'excerpt': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Rédiger un résume', 'rows':3}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Rédigez le contenu complet de votre article...', 'rows': 5}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'meta_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Titre SEO optimisé'}),
+            'meta_description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description SEO optimisée', 'rows': 3}),
+        }
 
