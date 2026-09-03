01d7cc1490377acf542e84901133300ca11c66fe
elpadrino94 <celestindjato5@gmail.com>
2026-09-03 12:25:57 +0000

    Ajout de la gestion des mises à jour et des suppressions d'articles et de catégories, ainsi que l'implémentation de modales de confirmation pour ces actions.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from dashboard.views import AddCategoryView, AddPostView, DashboardView, ListCategoriesView, PostsListView
+from dashboard.views import AddCategoryView, AddPostView, CategoryDeleteView, CategoryUpdateView, DashboardView, ListCategoriesView, PostsListView, PostUpdateView, PostDeleteView  
 
 
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path('add-post/', AddPostView.as_view(), name='add-post'),
     path('add-category/', AddCategoryView.as_view(), name='add-category'),
     path('categories/', ListCategoriesView.as_view(), name='categories-list'),
+    path('category/<str:slug>/update/', CategoryUpdateView.as_view(), name='update-category'),
+    path('category/<str:slug>/delete/', CategoryDeleteView.as_view(), name='delete-category'),
+    path('post/<str:slug>/update/', PostUpdateView.as_view(), name='update-post'),
+    path('post/<str:slug>/delete/', PostDeleteView.as_view(), name='delete-post'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Count
 
-from django.views.generic import CreateView, TemplateView, ListView
+from django.views.generic import CreateView, DeleteView, TemplateView, ListView, UpdateView
 from app.models import Category, Post
 from .forms import AddPostForm, CategoryAddForm
 from django.utils import timezone
@@ -39,7 +39,6 @@ class PostsListView(ListView):
         return context
 
 
-
 # ============================
 # ADD CATEGORY VIEW
 # ============================
@@ -48,8 +47,6 @@ class AddCategoryView(CreateView):
     form_class = CategoryAddForm
     template_name = 'dash/add_category.html'
     success_url = reverse_lazy('categories-list')
-
-
 
 # ==============================
 # ADD POST VIEW
@@ -85,3 +82,63 @@ class ListCategoriesView(ListView):
         context = super().get_context_data(**kwargs)
         context['total_categories'] = Category.objects.count()
         return context
+
+#===========================================
+# GESTION DES ACTIONS BOUTONS DE CATEGORIES
+#===========================================
+class CategoryUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Category
+    form_class = CategoryAddForm
+    template_name = 'dash/add_category.html'
+    success_url = reverse_lazy('categories-list')
+
+    def test_func(self):
+        return (
+            self.request.user.is_authenticated
+            and getattr(self.request.user, 'role', None) == 'admin'
+        )
+
+class CategoryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Category
+    template_name = 'dash/category_confirm_delete.html'
+    success_url = reverse_lazy('categories-list')
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def test_func(self):
+        return (
+            self.request.user.is_authenticated
+            and getattr(self.request.user, 'role', None) == 'admin'
+        )
+
+#===========================================
+# GESTION DES ACTIONS BOUTONS DES ARTICLES
+#===========================================
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = AddPostForm
+    template_name = 'dash/add_post.html'
+    success_url = reverse_lazy('posts-list')
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def test_func(self):
+        return (
+            self.request.user.is_authenticated
+            and getattr(self.request.user, 'role', None) == 'admin'
+        )
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'dash/post_confirm_delete.html'
+    success_url = reverse_lazy('posts-list')
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def test_func(self):
+        return (
+            self.request.user.is_authenticated
+            and getattr(self.request.user, 'role', None) == 'admin'
+        )
+
+
